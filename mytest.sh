a043405b43468896bf7e13e1716e2dacd3041494
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-03-11 18:15:46 +0530

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -1,5 +1,9 @@
 echo "This was a first practical of Jenkins"
 echo "To run shell script commands"
+echo "Print 5 times calender"
+echo "**************************"
 for((i=0;i<5;i++)); do cal; done
+echo "Print 5 times date"
+echo "**************************"
 for((i=0;i<5;i++)); do date; done
 echo "bye"
