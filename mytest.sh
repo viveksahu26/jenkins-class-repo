0fb48ab359811f3b9ee436d753fe65e8bc382ac5
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-03-11 17:52:10 +0530

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -1,5 +1,5 @@
 echo "This was a first practical of Jenkins"
 echo "To run shell script commands"
-for ((i=0; i<5; i++));do cal; done
-for ((i=0; i<5; i++));do date; done
+for((i=0; i<5; i++));do cal; done
+for((i=0; i<5; i++));do date; done
 echo "bye"
