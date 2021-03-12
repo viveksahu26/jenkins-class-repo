5b4fdf404d4ca82fcf9bc262c01f3e7e2120fb83
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-03-13 02:22:46 +0530

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -2,11 +2,13 @@
 echo "********************************* WELCOME !!************************************"
 echo "This was a first practical of Jenkins"
 echo "To run shell script commands"
-echo "Print 5 times calender"
+echo "Print 3 times calender"
 echo "*********************************************************************************"
-for((i=0;i<5;i++)); do cal; done
+for((i=0;i<3;i++)); do cal; done
+echo "*********************************************************************************"
 echo "Print 5 times date"
 echo "**********************************************************************************"
 for((i=0;i<5;i++)); do date; done
+echo "*********************************************************************************"
 echo "bye"
 echo " **************** BYEEeeeeeeeeeeeeeeeeeeeee ***************************************"
